625555c8af9feb574b4a138d26c16dbae860a3bb
Riya Dey <riyadey.0724@gmail.com>
2024-08-04 17:10:11 +0530

    Format prediction values upto 2 decimal places for better readability

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -104,7 +104,7 @@ class ModelTrainer:
 
             predicted = best_model.predict(X_test)
 
-            r2_square = round(r2_score(y_test, predicted), 2)  # Round r2_score to 2 decimal places
+            r2_square = round(r2_score(y_test, predicted), 2)
             return best_model_name, r2_square
 
         except Exception as e:
