9bf71581213a7c34d59428f95c11ea640ad88256
Riya Dey <riyadey.0724@gmail.com>
2024-08-04 17:01:48 +0530

    Prediction value format upto Format prediction values to two decimal places for improved readability2 decimal point

--- app.py
+++ app.py
@@ -35,7 +35,8 @@ def predict_datapoint():
         print("Mid Prediction")
         results=predict_pipeline.predict(pred_df)
         print("after Prediction")
-        return render_template('home.html',results=results[0])
+        rounded_results = [round(result, 2) for result in results]
+        return render_template('home.html',results=rounded_results[0])
     
 
 if __name__=="__main__":
